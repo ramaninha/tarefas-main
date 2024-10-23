28076c94709f5ffafedd21a81701cb50f35f935c
Ramana <maniandrade.o@gmail.com>
2024-10-23 16:07:31 -0300

    atividade p.i

--- tarefas-main/src/componentes/ListaDeTarefas/index.jsx
+++ tarefas-main/src/componentes/ListaDeTarefas/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import Tarefa from "../Tarefa";
+import Tarefa from "../Tarefa/index";
 
 function ListaDeTarefas() {
   const [tarefas, setTarefas] = useState([]);
@@ -39,6 +39,14 @@ function ListaDeTarefas() {
     buscarDados();
   }, []);
 
+  function onCompletaChange(tarefaId, novacompleta) {
+    setTarefas(
+      tarefas.map((tarefa) =>
+        tarefa.id === tarefaId ? { ...tarefa, completed: novacompleta}:tarefa
+      )
+    );
+  }
+
   return (
     <div style={{ padding: "20px" }}>
       {usuarios.map((usuario) => {
@@ -58,7 +66,7 @@ function ListaDeTarefas() {
               <h3>Tarefas Incompletas</h3>
               <div style={{ backgroundColor: "red", padding: "10px", color: "white" }}>
                 {incompletas.map((tarefa) => (
-                  <Tarefa key={tarefa.id} tarefa={tarefa} nomeUsuario={usuario.name} />
+                  <Tarefa key={tarefa.id} tarefa={tarefa} nomeUsuario={usuario.name} onCompletaChange={onCompletaChange} />
                 ))}
               </div>
             </div>
--- tarefas-main/src/componentes/Tarefa/index.jsx
+++ tarefas-main/src/componentes/Tarefa/index.jsx
@@ -1,18 +1,27 @@
-function Tarefa({ tarefa, nomeUsuario }) {
+function Tarefa(props) {
   const estiloTarefa = {
-    backgroundColor: tarefa.completed ? "green" : "red",
-    textDecoration: tarefa.completed ? "underline" : "none",
+    backgroundColor: props.tarefa.completed ? "green" : "red",
+    textDecoration: props.tarefa.completed ? "underline" : "none",
   };
 
   const renderTitulo = (title) => {
-    return tarefa.completed ? <u>{title}</u> : <span>{title}</span>;
+    return props.tarefa.completed ? <u>{title}</u> : <span>{title}</span>;
   };
 
+  const handleClick = () =>{
+    const newCompleta = !props.tarefa.completed;
+    //use a callback function to update the value of completa//
+    console.log(props)
+    props.onCompletaChange(props.tarefa.id, newCompleta);
+  }
+
   return (
-    <div>
-      <h4>{nomeUsuario}</h4>
+    
+    <div onClick={!props.tarefa.completed ? handleClick : undefined}>
+    
+      <h4>{props.nomeUsuario}</h4>
       <p style={estiloTarefa}>
-        {renderTitulo(tarefa.title)}
+        {renderTitulo(props.tarefa.title)}
       </p>
     </div>
   );
